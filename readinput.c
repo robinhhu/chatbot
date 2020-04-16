94682a38daa31a19c7ee236f2db47675e38ffd62
robinhhu <emeraldhu@icloud.com>
2020-04-16 20:16:14 +0800

    read input

--- readinput.c
+++ readinput.c
@@ -3,31 +3,111 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "readinput.h"
 
 // Remove the punctuation in the initial input
-void removePunctuation(char initialinput[])
+// And change the input into lower case
+void normalizeInput(char initialinput[])
 {
+	char *input = initialinput, *output = initialinput;
+	while(*input)
+	{
+		if(ispunct(*input))
+			input ++;
+		else if(isupper(*input))
+			*output++ = tolower(*input++);
+		else
+			*output ++ = *input ++;
+	}
+	*output = 0;
 	return;
 }
 
 // Split a lone sentence into list of words
-char *splitWords(char input[])
+// Return words number
+// Return -1 if something goes wrong
+int splitWords(char output[][20], char input[])
 {
-	return NULL;
+	int count = 0;
+	char temp[20];
+	if(input == NULL || strlen(input) == 0)
+		return -1;
+	int i = 0,index = 0;
+	while(1)
+	{
+		if(input[i] != ' ' && input[i] != ',' && input[i] != '\0')
+			temp[index ++] = input[i];
+		else if(index != 0)
+		{
+			temp[index] = '\0';
+			strcpy(output[count ++],temp);
+			index = 0;
+		}
+		if(input[i ++] == '\0')
+			break;
+	}
+	return count;
 }
+
+// If the source string end with the end string,
+// return the index the first letter in end appears
+// else return -1
+int endWith(char *src, char *end)
+{
+	if (strlen(end) > strlen(src))
+        	return -1;
+	if(strncmp(src + strlen(src) - strlen(end), end, strlen(end)) == 0)
+		return strlen(src) - strlen(end);
+	else
+		return -1;
+}
+
 
 // Stem words. E.g. change "having" to "hav"
 void stemWords(char input[])
 {
-	return;
+	int ing = endWith(input,"ing");
+	int s = endWith(input,"s");
+	int d = endWith(input,"d");
+	int e = endWith(input,"e");
+	if(ing != -1) 
+	{
+		if(input[ing - 1] == input[ing - 2])
+			input[ing - 1] = '\0';
+		else
+			input[ing] = '\0';
+	}
+	else if(s != -1 && strlen(input) > 2)
+	{
+		int es = endWith(input,"es");
+		if(es != -1)
+			input[es] = '\0';
+		else
+			input[s] = '\0';		
+	}
+	else if(d != -1)
+	{
+		int ed = endWith(input,"ed");
+		if(ed != -1)
+			input[ed] = '\0';
+		else
+			input[s] = '\0';	
+	}
+	else if(e != 1 && input[e - 1] != 'a' && input[e - 1] != 'e' && input[e - 1] != 'i' && input[e - 1] != 'o' && input[e - 1] != 'u' && (input[e - 2] == 'a' || input[e - 2] == 'e' || input[e - 2] == 'i' || input[e - 2] == 'o' || input[e - 2] == 'u') && strlen(input) < 5)
+		input[e] = '\0';
+	return;	
 }
 
-// Change uppercase words into lower case
-// While keep words in lower case still
-void toLowerCase(char uppercase[])
+// Change input into list of modified word list
+int modifyInput(char output[][20], char input[])
 {
-	return;
+	normalizeInput(input);
+	int wordsCount = splitWords(output,input);
+	int i = 0;
+	for(;i < wordsCount;i ++)
+		stemWords(output[i]);
+	return wordsCount;
 }
 
 // Count the score for a given string list
--- readinput.h
+++ readinput.h
@@ -1,6 +1,7 @@
-void removePunctuation(char initialinput[]);
-char *splitWords(char input[]);
+void normalizeInput(char initialinput[]);
+int splitWords(char output[][20], char input[]);
+int endWith(char *src, char *end);
 void stemWords(char input[]);
-void toLowerCase(char uppercase[]);
+int modifyInput(char output[][20], char input[]);
 double countScore(char input[][20], Key wordList);
 int findMax(char input[][20]);
